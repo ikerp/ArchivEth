03a221fd052a777a9e4afd2d90e256c6ca51fb7f
Iker Prego <ikerprego@gmail.com>
2019-09-13 09:51:30 +0200

    feat: Add Archive.sol contract
    Add Submition struct, submitions mapping and checkFile and submitFile functions

---
+++ contracts/Archive.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+contract Archive {
+
+    struct Submition {
+        address submitter;
+        uint256 time;
+    }
+
+    mapping(bytes32 => Submition) submitions;
+
+    function checkFile(bytes32 file) public view returns(bool) {
+        if (submitions[file].submitter != address(0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function submitFile(bytes32 newFile, uint256 timeStamp) public {
+        submitions[newFile] = Submition(msg.sender, timeStamp);
+    }
+}
