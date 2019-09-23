8e9f02a924b2fea31e3fe0697d179033677eea59
Iker Prego <ikerprego@gmail.com>
2019-09-23 12:49:14 +0200

    feat: Remove ConvertLib and Metacoin contracts included in the truffle box

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const Archive = artifacts.require("Archive");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Archive);
 };
